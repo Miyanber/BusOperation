161006ba865944a7a0d68ca6b42fcad38b399be1
Miyanber <miyanba2984h@gmail.com>
2023-11-23 18:29:19 +0900

    update settings

--- busOperation/urls.py
+++ busOperation/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import static, settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("approach/", include("approach.urls")),
+    path('', include("approach.urls")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
