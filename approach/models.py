3a2ab2054c9b723b78eafee3396da0178ad717ac
Miyanber <miyanba2984h@gmail.com>
2024-02-03 15:53:42 +0900

    エラー修正

--- approach/models.py
+++ approach/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import re
 
 # Create your models here.
 class Busstop(models.Model):
@@ -13,15 +14,36 @@ class Busstop(models.Model):
     pub_date = models.DateTimeField(auto_now=True)  # 作成日時
     order = models.PositiveSmallIntegerField(default=1)
     
+    destinations = None
+    
+    def setDestinations(self, destinations):
+        self.destinations = destinations
+    
     def __str__(self):
         return f"{self.order}.{self.busstop_name}({self.bus_company})"
 
 
 class Destination(models.Model):
     busstop = models.ForeignKey(Busstop, on_delete=models.CASCADE)  # どのバス停に対する行先か
-    destination_name = models.CharField(blank=True, max_length=64)  # 行先の選択肢python manage.py createsuperuser
+    destination_name = models.CharField(blank=True, max_length=64)  # 行先の名称
     approach_url = models.URLField(blank=True, max_length=1000)
     order = models.PositiveSmallIntegerField(default=1)
+    systems = None
+    
+    def setProperName(self):
+        if self.busstop.bus_company != Busstop.Company.KYOTO_CITY:
+            return ""
+        self.systems = self.getSystemsByName()
+        if self.systems:
+            self.destination_name = self.destination_name.replace(f"({self.systems})", "")
+            print(self.destination_name)
+        return ""
+    
+    def getSystemsByName(self):
+        if self.systems == None:
+            matchObj = re.search('([A-Z0-9,]{1,})', self.destination_name)
+            if(matchObj != None):
+                return matchObj.group()
     
     def __str__(self):
         return f"{self.order}.{self.busstop.busstop_name}_{self.destination_name}"
--- approach/views.py
+++ approach/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Subquery, OuterRef, JSONField
 from django.db import models
+from django.views.generic import ListView
 
 from .models import Busstop, Destination
 
@@ -12,17 +13,28 @@ def index(request):
     #         Destination.objects.filter(busstop__pk=OuterRef("pk")).order_by("order").values_list("pk", flat=True).__len__()
     #     )
     # )
-    busstop_dicts = []
     
     for busstop in busstops :
-        busstop_dict = {
-            "busstop": busstop,
-            "destinations": Destination.objects.filter(busstop__pk=busstop.pk).order_by("order").all()
-            }
-        busstop_dicts.append(busstop_dict) 
-    
+        busstop.setDestinations(Destination.objects.filter(busstop__pk=busstop.pk).order_by("order"))
+        
     context = {
-        "busstop_dicts": busstop_dicts,
+        "busstops": busstops,
         "Company": Busstop.Company
     }
     return render(request, "approach/index.html", context)
+
+# クラスベースのビュー(未実装)
+class IndexView(ListView):
+    template_name = 'approach/index.html'
+    model = Busstop
+    context_object_name = 'busstops'
+    # paginate_by = 10 取得制限
+    
+    def get_context_data(self):
+        ctx = super().get_context_data()
+
+        ctx['destinations'] = ctx['busstops']
+
+        return ctx
+    
+index_class = IndexView.as_view()
