2b327494b4df411b95271797d8cddb6f3892f005
Miyanber <miyanba2984h@gmail.com>
2024-11-10 18:45:21 +0900

    再トライ２

--- busOperation/settings.py
+++ busOperation/settings.py
@@ -144,9 +144,6 @@ STORAGES = {
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
 
 
 # Default primary key field type
